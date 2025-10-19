56e1cbd50b73f825d2415a1525b8758b0868001c
mounishwaran <mounish@gmail.com>
2025-10-19 12:38:34 +0530

    Ensure Content-Type JSON in axios interceptor

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -11,6 +11,8 @@ api.interceptors.request.use((config) => {
   if (token) {
     config.headers.Authorization = `Bearer ${token}`
   }
+
+  config.headers['Content-Type'] = 'application/json'
   return config
 })
 
